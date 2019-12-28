945282e722f47823e34e44c1d8d276b4c978b6bf
stbtpersonal <stbt.personal@gmail.com>
2019-12-28 10:41:18 +0200

    Implemented fetching relevant files from Google Drive.

--- app/src/main/java/io/github/stbtpersonal/srsschedule/MainActivity.kt
+++ app/src/main/java/io/github/stbtpersonal/srsschedule/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.stbtpersonal.srsschedule
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,7 @@ import com.google.android.gms.common.api.Scope
 import com.google.api.client.extensions.android.http.AndroidHttp
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.SheetsScopes
@@ -50,6 +52,7 @@ class MainActivity : Activity() {
     private fun requestSignIn() {
         val signInOptions = GoogleSignInOptions
             .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
             .requestScopes(
                 Scope(SheetsScopes.SPREADSHEETS),
                 Scope(DriveScopes.DRIVE_METADATA_READONLY)
@@ -75,8 +78,11 @@ class MainActivity : Activity() {
                         val credential = GoogleAccountCredential.usingOAuth2(this, scopes)
                         credential.selectedAccount = account.account
 
-                        this.populateSchedule(credential)
-                        this.showSchedule()
+                        object : Thread() {
+                            override fun run() {
+                                this@MainActivity.populateSchedule(credential)
+                            }
+                        }.start()
                     }
                     .addOnFailureListener { e ->
                         throw Exception(e)
@@ -88,9 +94,27 @@ class MainActivity : Activity() {
     private fun populateSchedule(credential: GoogleAccountCredential) {
         val jsonFactory = JacksonFactory.getDefaultInstance()
         val httpTransport = AndroidHttp.newCompatibleTransport()
-        val service = Sheets.Builder(httpTransport, jsonFactory, credential)
+
+        val drive = Drive.Builder(httpTransport, jsonFactory, credential)
             .setApplicationName(getString(R.string.app_name))
             .build()
+
+        val result = drive
+            .files()
+            .list()
+            .setFields("files(id, name, webViewLink)")
+            .setQ("mimeType = 'application/vnd.google-apps.spreadsheet' and name contains '[\"SRS\"]'")
+            .execute()
+        val files = result.files
+        for (file in files) {
+            Log.d("kaka", file.name)
+        }
+
+        val sheets = Sheets.Builder(httpTransport, jsonFactory, credential)
+            .setApplicationName(getString(R.string.app_name))
+            .build()
+
+        this.runOnUiThread { this.showSchedule() }
     }
 
     private fun hideSchedule() {
