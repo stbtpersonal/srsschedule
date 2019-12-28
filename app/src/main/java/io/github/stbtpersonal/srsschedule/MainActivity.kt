c5228204cb51cd9ba400fddb79b5a21c8cdd03be
stbtpersonal <stbt.personal@gmail.com>
2019-12-28 10:07:28 +0200

    Implemented Google login.

--- app/src/main/java/io/github/stbtpersonal/srsschedule/MainActivity.kt
+++ app/src/main/java/io/github/stbtpersonal/srsschedule/MainActivity.kt
@@ -1,13 +1,28 @@
 package io.github.stbtpersonal.srsschedule
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Scope
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.drive.DriveScopes
+import com.google.api.services.sheets.v4.Sheets
+import com.google.api.services.sheets.v4.SheetsScopes
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : Activity() {
+    companion object {
+        private const val GOOGLE_SIGN_IN_REQUEST = 1
+    }
+
     private val scheduleRecyclerViewAdapter = ScheduleRecyclerViewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +30,11 @@ class MainActivity : Activity() {
 
         this.setContentView(R.layout.activity_main)
 
-        this.scheduleRecyclerView.layoutManager =
-            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        this.scheduleRecyclerView.layoutManager = LinearLayoutManager(
+            this,
+            RecyclerView.VERTICAL,
+            false
+        )
         this.scheduleRecyclerView.adapter = this.scheduleRecyclerViewAdapter
 
         NotificationScheduler.scheduleNotifications(this)
@@ -25,6 +43,63 @@ class MainActivity : Activity() {
     }
 
     private fun refresh() {
+        this.hideSchedule()
+        this.requestSignIn()
+    }
 
+    private fun requestSignIn() {
+        val signInOptions = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(
+                Scope(SheetsScopes.SPREADSHEETS),
+                Scope(DriveScopes.DRIVE_METADATA_READONLY)
+            )
+            .build()
+        val client = GoogleSignIn.getClient(this, signInOptions)
+
+        this.startActivityForResult(client.signInIntent, GOOGLE_SIGN_IN_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                GoogleSignIn
+                    .getSignedInAccountFromIntent(data)
+                    .addOnSuccessListener { account ->
+                        val scopes = listOf(
+                            SheetsScopes.SPREADSHEETS,
+                            DriveScopes.DRIVE_METADATA_READONLY
+                        )
+                        val credential = GoogleAccountCredential.usingOAuth2(this, scopes)
+                        credential.selectedAccount = account.account
+
+                        this.populateSchedule(credential)
+                        this.showSchedule()
+                    }
+                    .addOnFailureListener { e ->
+                        throw Exception(e)
+                    }
+            }
+        }
+    }
+
+    private fun populateSchedule(credential: GoogleAccountCredential) {
+        val jsonFactory = JacksonFactory.getDefaultInstance()
+        val httpTransport = AndroidHttp.newCompatibleTransport()
+        val service = Sheets.Builder(httpTransport, jsonFactory, credential)
+            .setApplicationName(getString(R.string.app_name))
+            .build()
+    }
+
+    private fun hideSchedule() {
+        this.spinner.visibility = View.VISIBLE
+        this.scheduleContainer.visibility = View.GONE
+    }
+
+    private fun showSchedule() {
+        this.spinner.visibility = View.GONE
+        this.scheduleContainer.visibility = View.VISIBLE
     }
 }
